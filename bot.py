53dacdcc88113effab74aad85847c0b1bbe57771
Евгений <volga52@bk.ru>
2022-12-28 18:01:24 +0300

    Organization of the bot.py file

--- bot.py
+++ bot.py
@@ -1,1 +1,56 @@
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
+import asyncio
+import logging
 
+from tg_bot.config import load_config
+
+
+logger = logging.getLogger(__name__)
+
+
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s'
+    )
+    config = load_config(".env")
+
+    bot = Bot(token=config.tg_bot.token, parse_mode="HTML")
+    storage = RedisStorage2() if config.tg_bot.use_redis else MemoryStorage
+    dp = Dispatcher(bot, storage=storage)
+    bot['config'] = config
+
+    register_all_middlewares(dp)
+    register_all_filters(dp)
+    register_all_handlers(dp)
+
+    try:
+        await dp.start_polling()
+    except:
+        pass
+    finally:
+        await dp.storage.close()
+        await dp.storage.wait_closed()
+        await bot.close()
+
+
+def register_all_middlewares(dp):
+    dp.setup_middleware()
+
+
+def register_all_filters(dp):
+    dp.filters_factory()
+
+
+def register_all_handlers(dp):
+    # register_admin()
+    pass
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logger.error('Bot stopping')
